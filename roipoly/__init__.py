a76c40d5ea90399bbf7409253c49dd920ce87023
HeinzJiao <151459908+HeinzJiao@users.noreply.github.com>
2024-09-28 14:17:23 +0200

    Delete roipoly directory

--- roipoly/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import add_roipoly_config
-from .detector import RoIPoly
-from .dataset_mapper import RoIPolyDatasetMapper
-from .test_time_augmentation import RoIPolyWithTTA
-from .swintransformer import build_swintransformer_fpn_backbone
